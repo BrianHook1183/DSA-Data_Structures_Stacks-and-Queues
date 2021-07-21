ab0f78e47afc394cec33ae2415777e750850e633
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-21 17:49:09 -0500

    submitted soltuion

--- src/Queue.js
+++ src/Queue.js
@@ -12,11 +12,30 @@ class Queue {
   }
 
   enqueue(value) {
-    // your solution here
+    const newNode = new Node(value);
+
+    if (this.first) {
+      this.last.next = newNode;
+    } else {
+      // Set the node of the queue's next to be the new node
+      this.first = newNode;
+    }
+
+    //make the new node the last item on the queue
+    this.last = newNode;
   }
 
   dequeue() {
-    // your solution here
+    if (this.first) {
+      const dequeued = this.first;
+      this.first = dequeued.next;
+
+      if (dequeued === this.last) {
+        this.last = null;
+      }
+
+      return dequeued.value;
+    }
   }
 }
 
